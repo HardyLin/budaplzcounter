a00ddaf975245fe1df1d82cf5191793fa75c6416
HardyLin <hardylinjob@gmail.com>
2023-07-19 16:18:10 +0800

    fix: fix the var bug change part of user to userdata and login will success

--- run.py
+++ run.py
@@ -8,9 +8,10 @@ mp3files="static/budaplz.mp3"
 app=Flask(__name__)
 config = app.config
 app.secret_key = config.get('flask', '730713acad5673b116152920fe534dc6')
+
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.session_protection = "strong"
+# login_manager.session_protection = "strong"
 login_manager.login_view = 'login'
 login_manager.login_message = '請證明你並非來自黑暗草泥馬界'
 
@@ -78,20 +79,19 @@ def login():
     if request.method == 'GET':
         return render_template("login.html")
     
-    user = request.form['user_id']
-    if (user in users) and (request.form['password'] == users[user]['password']):
+    userdata = request.form['user_id']
+    if (userdata in users) and (request.form['password'] == users[userdata]['password']):
         user = User()
-        user.id = user
+        user.id = userdata
         login_user(user)
-        flash(f'你好{user}!')
-
+        flash(f'{userdata}！歡迎加入草泥馬訓練家的行列！')
         return redirect(url_for('home'))
-    else:
-        print(487878)
-        flash('登入失敗了...')
-        return render_template('login.html')
+
+    flash('登入失敗了...')
+    return render_template('login.html')
 
 @app.route('/logout')
+@login_required
 def logout():
     userdata = current_user.get_id()
     logout_user()
@@ -99,7 +99,7 @@ def logout():
     return render_template('login.html')
 
 @app.route('/')
-# @login_required
+@login_required
 def home():
     defaultData = query_db('select * from counterrecord')[-1]
     print(defaultData)
